95d49809da37f90dee7a81e98ee479211859b975
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-04-07 00:05:03 +0200

    feat: :sparkles: Implement Expense model class with properties

--- src/main/java/com/fairshare/model/Expense.java
+++ src/main/java/com/fairshare/model/Expense.java
@@ -1,4 +1,11 @@
 package com.fairshare.model;
 
 public class Expense {
+    private int id;
+    private String name;
+    private ExpenseCategory category;
+    private double amount;
+    private User paidBy;
+    private Group group;
+
 }
