dd454a56e55a3c9ff8347ec5c17b0c756df3accc
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-04-15 18:07:20 +0200

    feat: :sparkles: Enhance GroupController and UserController with additional methods for user and group management

--- src/main/java/com/fairshare/controller/GroupController.java
+++ src/main/java/com/fairshare/controller/GroupController.java
@@ -1,4 +1,38 @@
 package com.fairshare.controller;
 
+import com.fairshare.model.Group;
+import com.fairshare.service.GroupService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/groups")
 public class GroupController {
+
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Group> getAllGroups(){
+        return groupService.getAllGroups();
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Group> getGroup(@PathVariable Long id){
+        return groupService.getGroup(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Group createGroup(@RequestBody Group group){
+        return groupService.createGroup(group);
+    }
+
 }
--- src/main/java/com/fairshare/controller/UserController.java
+++ src/main/java/com/fairshare/controller/UserController.java
@@ -1,4 +1,42 @@
 package com.fairshare.controller;
 
+import com.fairshare.model.User;
+import com.fairshare.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService service) {
+        this.userService = service;
+    }
+
+    //TODO: DTO's toevoegen
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<User> getUser(@PathVariable Long id) throws Exception {
+        var foundUser = userService.getUser(id);
+        if (foundUser.isEmpty()) throw new Exception("User not found!");
+        return foundUser;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
 }
