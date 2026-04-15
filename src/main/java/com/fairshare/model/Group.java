92f4d5ff7b049a0cf84dd17867d2caa8aff4cb9f
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-04-15 18:07:09 +0200

    feat: :sparkles: Add constructors to Group and User models

--- src/main/java/com/fairshare/model/Group.java
+++ src/main/java/com/fairshare/model/Group.java
@@ -2,13 +2,17 @@ package com.fairshare.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="groups")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Group {
 
     @Id
@@ -18,4 +22,9 @@ public class Group {
     @OneToMany
     @JoinColumn(name = "user_id")
     private List<User> users;
+
+    public Group(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/main/java/com/fairshare/model/User.java
+++ src/main/java/com/fairshare/model/User.java
@@ -2,14 +2,19 @@ package com.fairshare.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
 }
