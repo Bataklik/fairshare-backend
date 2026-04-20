ce84ecdcd68e2da8d1bdc83ca680704ca3a5911f
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-04-20 11:25:28 +0200

    feat: add method to find expenses by user who paid

--- src/main/java/com/fairshare/repository/ExpenseRepository.java
+++ src/main/java/com/fairshare/repository/ExpenseRepository.java
@@ -1,7 +1,13 @@
 package com.fairshare.repository;
 
 import com.fairshare.model.Expense;
+import com.fairshare.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense,Long> {
+
+    List<Expense> findByPaidBy(User paidBy);
 }
