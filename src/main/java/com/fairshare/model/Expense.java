fd9a56640ae456c0a9a64924d28f666845410bdf
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-04-22 19:07:13 +0200

    feat: enhance Expense class with additional methods for managing splits

--- src/main/java/com/fairshare/model/Expense.java
+++ src/main/java/com/fairshare/model/Expense.java
@@ -2,6 +2,7 @@ package com.fairshare.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -46,4 +47,23 @@ public class Expense {
     @JoinColumn(name = "group_id")
     private Group group;
 
+    public Expense(String name, double amount, ExpenseCategory category, User paidBy,Group group) {
+        this.name = name;
+        this.amount = amount;
+        this.category = category;
+        this.paidBy = paidBy;
+        this.group = group;
+    }
+
+    public void addSplit(ExpenseSplit expenseSplit){
+        splits.add(expenseSplit);
+    }
+
+    public boolean removeSplitByExpenseSplit(ExpenseSplit expenseSplit){
+        return splits.remove(expenseSplit);
+    }
+    public boolean removeSplitById(Long expenseSplitId){
+        return getSplits()
+                .removeIf(ep -> Objects.equals(ep.getId(), expenseSplitId));
+    }
 }
