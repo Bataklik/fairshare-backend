68090c3e97086383bce561508c8a4a3f16424208
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-05-06 15:40:00 +0200

    feat: :sparkles: refactor color and theme management, add Typography class for consistent text styles

--- mobile/app/src/main/java/com/example/mobile/core/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/example/mobile/core/ui/theme/Color.kt
@@ -2,12 +2,12 @@ package com.example.mobile.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-class Color {
-    // core/ui/theme/Color.kt
 
-    val AppBackground = Color(0xFFF0EEE9)
-    val AppWhite = Color(0xFFFFFFFF)
-    val AppTextSecondary = Color(0xFF6E7672)
-    val AppButtonTeal = Color(0xFF2E8B7D)
-    val AppDark = Color(0xFF1B1F1D)
-}
+val AppBackground = Color(0xFFF0EEE9)
+val AppWhite = Color(0xFFFFFFFF)
+val AppTextGrey = Color(0xFF6E7672)
+val AppButtonGreen = Color(0xFF2E8B7D)
+val AppDark = Color(0xFF1B1F1D)
+
+val AppTealDark = Color(0xFF103A33)
+
--- mobile/app/src/main/java/com/example/mobile/core/ui/theme/Theme.kt
+++ mobile/app/src/main/java/com/example/mobile/core/ui/theme/Theme.kt
@@ -1,14 +1,27 @@
 package com.example.mobile.core.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
 
-class Theme {
-    private val LightColorScheme = lightColorScheme(
-        primary = AppButtonTeal,
-        onPrimary = AppWhite,
-        background = AppBackground,
-        onBackground = AppDark,
-        surface = AppWhite,
-        onSurface = AppTextSecondary
+private val LightColorScheme = lightColorScheme(
+    primary = AppButtonGreen,
+    onPrimary = AppWhite,
+    background = AppBackground,
+    onBackground = AppDark,
+    surface = AppWhite,
+    onSurface = AppTextGrey,
+    tertiary = AppTealDark
+
+)
+
+@Composable
+fun FairShareTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = LightColorScheme,
+        typography = Typography,
+        content = content
     )
 }
