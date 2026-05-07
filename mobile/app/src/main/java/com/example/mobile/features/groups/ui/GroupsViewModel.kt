30959885178ca9a2d2f745c6f6e0cb73875410c6
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-05-07 16:29:44 +0200

    refactor: update GroupsViewModel to use mutableStateOf for group management and remove unused LiveData

--- mobile/app/src/main/java/com/example/mobile/features/groups/ui/GroupsViewModel.kt
+++ mobile/app/src/main/java/com/example/mobile/features/groups/ui/GroupsViewModel.kt
@@ -1,34 +1,49 @@
 package com.example.mobile.features.groups.ui
 
-import androidx.compose.ui.tooling.data.Group
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mobile.core.network.RetrofitClient
+import com.example.mobile.features.groups.domain.Group
 import kotlinx.coroutines.launch
 
-class GroupsViewModel : ViewModel(){
+class GroupsViewModel : ViewModel() {
 
-    private val _data = MutableLiveData("No data")
-    val data: MutableLiveData<String> = _data
+    var groupState by mutableStateOf<List<Group>>(emptyList())
+        private set
+    var selectedGroup by mutableStateOf<Group?>(null)
+        private set
 
     init {
         viewModelScope.launch {
-            getGroup(1)
+            getGroups()
+            print(groupState)
         }
     }
 
-    private suspend fun getGroups(){
-        _data.value = RetrofitClient
-            .groupApiService
-            .getGroups()
-            .toString()
+    private suspend fun getGroups() {
+        try {
+            val groups = RetrofitClient
+                .groupApiService
+                .getGroups()
+
+            groupState = groups
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
-    private suspend fun getGroup(id: Int){
-        _data.value = RetrofitClient
-            .groupApiService
-            .getGroup(id)
-            .toString()
+    private suspend fun getGroup(id: Int) {
+        try {
+            val group = RetrofitClient
+                .groupApiService
+                .getGroup(id)
+
+            selectedGroup = group
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
